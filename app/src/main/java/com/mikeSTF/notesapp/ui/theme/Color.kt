5c3029f4d97b678c81e5a0cd953c8d91e042a53d
Miguel Angel Miranda Olvera <miguel_angelstf@outlook.com>
2023-09-08 18:28:57 -0600

    Feature: Se agregan Recursos de Font, Theme, Colors e iconos al proyecto.

--- app/src/main/java/com/mikeSTF/notesapp/ui/theme/Color.kt
+++ app/src/main/java/com/mikeSTF/notesapp/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.mikeSTF.notesapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val RedButton = Color(0xFFFF4646)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlackShade = Color(0xFF252525)
+val White = Color(0xFFFFFFFF)
+
+// Note Item Colors
+val RichBrilliantLavender = Color(0xFFFD99FF)
+val LightSalmonPink = Color(0xFFFF9E9E)
+val LightGreen = Color(0xFF91F48F)
+val PastelYellow = Color(0xFFFFF599)
+val Waterspout = Color(0xFF9EFFFF)
+val PaleViolet = Color(0xFFB69CFF)
+
+// Icons Background Color
+val BlackOlive = Color(0xFF3B3B3B)
+
+// Text Color
+val ChineseSilver = Color(0xFFCCCCCC)
