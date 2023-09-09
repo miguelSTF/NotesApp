74dc51ab39b65e4e7371b7a9aba3b1f39a0a882d
Miguel Angel Miranda Olvera <miguel_angelstf@outlook.com>
2023-09-08 18:38:12 -0600

    Feature: Se agregan las librerias y componentes necesarios para la app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
@@ -9,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mikeSTF.notesapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -23,15 +25,12 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -40,9 +39,9 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.1.1"
     }
-    packaging {
+    packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -51,19 +50,44 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation ("androidx.activity:activity-compose:1.6.1")
+    implementation ("androidx.compose.ui:ui:1.3.2")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.3.2")
+    implementation ("androidx.compose.material:material:1.3.1")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.3.2")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.3.2")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.3.2")
+
+    // Compose Navigation
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+
+    // Room Components
+    implementation ("androidx.room:room-runtime:2.4.3")
+    kapt ("androidx.room:room-compiler:2.4.3")
+    implementation ("androidx.room:room-ktx:2.4.3")
+    implementation ("androidx.room:room-paging:2.4.3")
+
+    // Dagger - Hilt
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+
+    // Splash Screen
+    implementation ("androidx.core:core-splashscreen:1.0.0")
+
+    // Paging
+    implementation ("androidx.paging:paging-compose:1.0.0-alpha17")
+
+    // App Startup
+    implementation ("androidx.startup:startup-runtime:1.1.1")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
